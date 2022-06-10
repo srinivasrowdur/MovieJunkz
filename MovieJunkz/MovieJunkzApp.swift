11be4fc67c339ccb522fc869ac06069b248ae06a
srinivasrowdur <99046278+srinivasrowdur@users.noreply.github.com>
2022-06-10 07:32:10 +0100

    Initial Commit

---
+++ MovieJunkz/MovieJunkzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieJunkzApp.swift
+//  MovieJunkz
+//
+//  Created by Srinivas Rowdur on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieJunkzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
